113ed73f5e608a5f393d56903628347dbbd80dd7
IDY KANGELA Landry <idylandry03@gmail.com>
2026-05-23 01:15:18 +0100

    Ajout DAO et séparation des responsabilités

---
+++ dao/seance_dao.py
@@ -0,0 +1,6 @@
+class SeanceDAO:
+    def get_all(self):
+        return [
+            {"id": 1, "date": "2026-05-10", "heure": "08:00-10:00", "salle": "B12"},
+            {"id": 2, "date": "2026-05-11", "heure": "10:00-12:00", "salle": "Labo Info"},
+        ]
--- services/gestionnaire_calendrier.py
+++ services/gestionnaire_calendrier.py
@@ -1,19 +1,8 @@
+from dao.seance_dao import SeanceDAO
+
 class GestionnaireCalendrier:
     def __init__(self):
-        self.seances = [
-            {
-                "id": 1,
-                "date": "2026-05-10",
-                "heure": "08:00-10:00",
-                "salle": "B12"
-            },
-            {
-                "id": 2,
-                "date": "2026-05-11",
-                "heure": "10:00-12:00",
-                "salle": "Labo Info"
-            }
-        ]
+        self.dao = SeanceDAO()
 
     def get_all_seances(self):
-        return self.seances
+        return self.dao.get_all()
