0bb399dee336bdd592e2105579b0f7ec5cf386bc
IDY KANGELA Landry <idylandry03@gmail.com>
2026-05-23 01:00:33 +0100

    Organisation de la structure Flet

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import flet as ft
 from services.gestionnaire_calendrier import GestionnaireCalendrier
 
-
 def main(page: ft.Page):
     page.title = "SmartCalendar"
 
     gestionnaire = GestionnaireCalendrier()
     seances = gestionnaire.get_all_seances()
 
-    page.add(
-        ft.Text("Calendrier des séances", size=20, weight="bold")
-    )
+    page.add(ft.Text("Calendrier des séances", size=20, weight="bold"))
 
     for s in seances:
         page.add(
@@ -18,6 +15,5 @@ def main(page: ft.Page):
                 f"ID: {s['id']} | Date: {s['date']} | {s['heure']} | Salle: {s['salle']}"
             )
         )
-
 
 ft.run(main)
