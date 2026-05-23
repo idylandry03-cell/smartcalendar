777565a7ba5d23a3b5000d8e13ca20dbf9e9e888
IDY KANGELA Landry <idylandry03@gmail.com>
2026-05-23 02:38:23 +0100

    update

--- dao/seance_dao.py
+++ dao/seance_dao.py
@@ -1,24 +1,6 @@
-from dao.base_dao import BaseDAO
-from modeles.models import Seance, TypeSeance
-
-
-class SeanceDAO(BaseDAO):
-    def __init__(self):
-        super().__init__()
-
-        self.save(
-            Seance(
-                1, "2026-05-10", "08:00", "10:00",
-                "Salle B12", False, 1, TypeSeance.COURS_MAGISTRAL
-            )
-        )
-
-        self.save(
-            Seance(
-                2, "2026-05-11", "10:00", "12:00",
-                "Labo Informatique", False, 1, TypeSeance.TP
-            )
-        )
-
-    def get_by_date(self, date):
-        return [s for s in self.data if s.date_seance == date]
+class SeanceDAO:
+    def get_all(self):
+        return [
+            {"id": 1, "date": "2026-05-10", "heure": "08:00-10:00", "salle": "B12"},
+            {"id": 2, "date": "2026-05-11", "heure": "10:00-12:00", "salle": "Labo Info"},
+        ]
